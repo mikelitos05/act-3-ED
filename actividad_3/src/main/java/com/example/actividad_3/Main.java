bdce8034375b7c798b9d087471d201a7323ea5bc
mikelitos05 <migueladalberto494@gmail.comz>
2025-09-09 15:42:29 -0600

    branch desde 0

--- actividad_3/src/main/java/com/example/actividad_3/Main.java
+++ actividad_3/src/main/java/com/example/actividad_3/Main.java
@@ -1,10 +1,8 @@
 package com.example.actividad_3;
-import com.example.actividad_3.ui.App;
 
 public class Main {
     public static void main(String[] args) {
-        App app = new App();
-        app.runSudokuGUI();   // ← SOLO esto
+    
     }
 }
 
